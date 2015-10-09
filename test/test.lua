d8d106910b1c17fc2f587d2483a704c2cf5f329b
opussf <github@zz9-za.com>
2015-10-09 12:23:48 -0700

    New test files to build out testing.

---
+++ test/test.lua
@@ -0,0 +1,20 @@
+#!/usr/bin/env lua
+
+addonData = { ["version"] = "1.0",
+}
+
+require "wowTest"
+
+test.outFileName = "testOut.xml"
+
+-- require the file to test
+package.path = "../src/?.lua;'" .. package.path
+require "AutoProfit"
+
+function test.before()
+end
+
+function test.after()
+end
+
+test.run()
